5b4bb85300ea442c1b15fc3996e90d273d4e8ddd
19taurus79 <19taurus79@gmail.com>
2025-09-07 12:36:49 +0300

    feat: add OpenAPI documentation and Swagger UI integration
    - Updated package.json to include build and preview scripts for API documentation.
    - Added OpenAPI specification in docs/openapi.yaml.
    - Generated Swagger JSON output in docs/swagger.json.
    - Created middleware for serving Swagger UI in src/middlewares/swaggerDocs.js.
    - Defined constants for Swagger path in src/constants/index.js.
    - Integrated Swagger UI into the server in src/server.js.
    - Added HTML template for ReDoc in docs/index.html.
    - Created response and schema definitions in swagger/components.
    - Defined API paths for contacts in swagger/paths.

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 dotenv.config();
 const PORT = Number(getEnvVar('PORT', 3000));
 export const startServer = () => {
@@ -28,9 +29,11 @@ export const startServer = () => {
     }),
   );
   app.use(router);
+  app.use('/api-docs', swaggerDocs());
   app.use(notFoundHandler);
   app.use(errorHandler);
   app.use('/uploads', express.static(UPLOAD_DIR));
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
